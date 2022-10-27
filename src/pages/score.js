51323f9d2ef2a31859de2ce925f9847e7742e241
Shiyun <lushiyun@outlook.com>
2022-10-27 12:25:27 -0400

    Complete score page

--- src/pages/score.js
+++ src/pages/score.js
@@ -1,16 +1,36 @@
-import { Box, Avatar, Text, Stack, Spinner } from '@chakra-ui/core';
+import { Box, Text, Stack, Spinner } from '@chakra-ui/core';
+import { collection, getDocs } from "firebase/firestore";
+import React from 'react';
 
 import Layout from '../components/Layout';
 import { Table, Tr, Th, Td } from '../components/Table';
-import useScores from '../util/useScores';
+import database from '../util/firebaseConfig';
 import parseSeconds from '../util/parseSeconds';
 
 const Score = () => {
-  const { data, isLoading, isError } = useScores();
+  const [scores, setScores] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const getScores = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(database, "scores"));
+        const scores = querySnapshot.docs.map((doc) => doc.data());
+        setScores(scores);
+      } catch (error) {
+        window.alert('Error fetching scores');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getScores();
+  }, []);
+  
 
   return (
     <Layout>
-      {isLoading ? (
+      {loading ? (
         <Spinner size="xl" label="loading scores..." />
       ) : (
         <Table w="full">
@@ -23,16 +43,15 @@ const Score = () => {
             </Tr>
           </thead>
           <tbody>
-            {data.data.map((score, index) => (
+            {scores.map((score, index) => (
               <Box as="tr" key={index}>
                 <Td>{index + 1}</Td>
                 <Td>
                   <Stack isInline spacing={2} align="center">
-                    <Avatar name={score.name} src={score.image} />
                     <Text fontSize="sm">{score.name}</Text>
                   </Stack>
                 </Td>
-                <Td>{score.points}</Td>
+                <Td>{score.score}</Td>
                 <Td>{parseSeconds(score.seconds)}</Td>
               </Box>
             ))}
