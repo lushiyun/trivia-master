fb5a9cd101aaa0af3490df7b7cdff7d01e400ce2
Shiyun <lushiyun@outlook.com>
2022-10-29 14:12:02 -0400

    change firebase setting for stackerblitz

--- src/util/firebaseConfig.js
+++ src/util/firebaseConfig.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,6 +18,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const database = getFirestore(app);
+const database = initializeFirestore(app, {
+  experimentalAutoDetectLongPolling: true,
+});
 
 export default database;
